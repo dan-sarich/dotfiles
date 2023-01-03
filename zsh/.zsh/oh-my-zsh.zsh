baf2c494b7ffe1676dfa1d24f10f50346c9afe28
Daniel Sarich <daniel_sarich@vfc.com>
2023-01-03 08:54:32 -0700

    Update mac config

--- zsh/.zsh/oh-my-zsh.zsh
+++ zsh/.zsh/oh-my-zsh.zsh
@@ -9,9 +9,6 @@ export ZSH="$HOME/.oh-my-zsh"
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 
-if [[ $TERM_PROGRAM = "Apple_Terminal" ]]; then
-  ZSH_THEME="half-life"
-fi
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
